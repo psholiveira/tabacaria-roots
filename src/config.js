c505156e1d921ed4a892f6e9ea8ef4fb2b45e1d8
Pedro Santos <pedroholiveira15@gmail.com>
2026-05-29 13:29:36 -0300

    feats: alteração do horário de funcionamento

--- src/config.js
+++ src/config.js
@@ -5,13 +5,13 @@ export const WHATSAPP_COLOR = '#25D366';
 
 // Horários por dia da semana (0=Dom, 1=Seg...6=Sab), em minutos desde meia-noite
 const HOURS_BY_DAY = [
-  { open: 14 * 60, close: 22 * 60 }, // Dom
-  { open: 10 * 60, close: 22 * 60 }, // Seg
-  { open: 10 * 60, close: 22 * 60 }, // Ter
-  { open: 10 * 60, close: 22 * 60 }, // Qua
-  { open: 10 * 60, close: 22 * 60 }, // Qui
-  { open: 10 * 60, close: 24 * 60 }, // Sex
-  { open: 10 * 60, close: 24 * 60 }, // Sab
+  { close: 9 * 60, close: 18 * 60 }, // Dom
+  { open: 9 * 60, close: 18 * 60 }, // Seg
+  { open: 9 * 60, close: 18 * 60 }, // Ter
+  { open: 9 * 60, close: 18 * 60 }, // Qua
+  { open: 9 * 60, close: 18 * 60 }, // Qui
+  { open: 9 * 60, close: 18 * 60 }, // Sex
+  { open: 9 * 60, close: 16 * 60 }, // Sab
 ];
 
 export function isStoreOpen() {
